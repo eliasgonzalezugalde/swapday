1ed881f33e9b24250a2f340798e9aa4a4d3732f7
eliasgonzalezugalde <eliasgonzalezugalde@gmail.com>
2016-04-17 23:45:23 -0600

    primer commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name swapdayApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the swapdayApp
+ */
+angular.module('swapdayApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name swapdayApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the swapdayApp
+ */
+angular.module('swapdayApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
